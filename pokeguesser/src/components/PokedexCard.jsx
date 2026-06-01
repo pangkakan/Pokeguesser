bd5880e6f445a776fc925faa357f9128fe24b87c
Williamstara <staraa.william@gmail.com>
2026-06-01 20:17:56 +0200

    Updated CSS
    Updated CSS and visuals to be more appealing to the eye.

--- pokeguesser/src/components/PokedexCard.jsx
+++ pokeguesser/src/components/PokedexCard.jsx
@@ -8,41 +8,40 @@ export default function PokedexCard({ pokemon }) {
                 alt={pokemon.name} className="w-full h-48 object-contain bg-gray-100"
             />
 
-            <h3 className="font-bold text-lg capitalize mt-2">{pokemon.name}</h3>
+            <h3 className="font-bold text-lg capitalize mt-2 text-center">{pokemon.name}</h3>
 
-            <div>Exp: {pokemon.base_experience}</div>
-
-            <div className="mt-2">
-                <p className="font-semibold">Attacks: </p>
-                <ul className="list-disc ml-4">
-                    {pokemon.moves?.slice(0, 5).map(move => (
-                        <li key={move.move.name} className="capitalize">
-                            {move.move.name}
-                        </li>
-                    ))}
-                </ul>
+            <div className="text-center text-m mt-2">
+                <p>Exp: {pokemon.base_experience} | Height: {(pokemon.height / 10).toFixed(1)} m | Weight: {(pokemon.weight / 10).toFixed(1)} kg</p>
             </div>
 
-            <div className="mt-2">
-                <p className="font-semibold">Abilities: </p>
-                <ul className="list-disc ml-4">
-                    {pokemon.abilities?.slice(0, 5).map(a => (
-                        <li key={a.ability.name} className="capitalize">
-                            {a.ability.name}
-                        </li>
-                    ))}
-                </ul>
+            <div className="grid grid-cols-2 gap-2 mt-3">
+                <div className="mt-2">
+                    <p className="font-semibold">Attacks: </p>
+                    <ul className="list-disc ml-4">
+                        {pokemon.moves?.slice(0, 6).map(move => (
+                            <li key={move.move.name} className="capitalize">
+                                {move.move.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="mt-2">
+                    <p className="font-semibold">Stats: </p>
+                    <ul className="list-disc ml-4">
+                        {pokemon.stats?.map(stat => (
+                            <li key={stat.stat.name} className="capitalize">
+                                {stat.stat.name}: {stat.base_stat}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
 
-            <div className="mt-2">
-                <p className="font-semibold">Stats: </p>
-                <ul className="list-disc ml-4">
-                    {pokemon.stats?.map(stat => (
-                        <li key={stat.stat.name} className="capitalize">
-                            {stat.stat.name}: {stat.base_stat}
-                        </li>
-                    ))}
-                </ul>
+            <div className="text-center text-m mt-3">
+                <p>
+                    Abilities: {pokemon.abilities?.map(a => a.ability.name).join(" | ")}
+                </p>
             </div>
 
         </div>
