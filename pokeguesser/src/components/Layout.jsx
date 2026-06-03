cdfc9d5769c75a97990033409db623930131d89f
Munther23 <jonathan.munther@gmail.com>
2026-06-03 17:55:37 +0200

    Update Layout.jsx

--- pokeguesser/src/components/Layout.jsx
+++ pokeguesser/src/components/Layout.jsx
@@ -3,8 +3,7 @@ import Navbar from './Navbar'
 export default function Layout({ children }) {
   return (
    <div
-      className="min-h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/pikachu.jpg')" }}
+      className="min-h-screen bg-white"
     >      <Navbar/>
       {children}
     </div>
