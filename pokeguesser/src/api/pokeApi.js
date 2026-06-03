296bb1502b3b6a19637ec1dad61d738308796ee0
Munther23 <jonathan.munther@gmail.com>
2026-06-03 17:15:17 +0200

    fix/uniform-api-calls
    Refactored the api calls in Search.jsx to use the methods in the pokeApi.js

--- pokeguesser/src/api/pokeApi.js
+++ pokeguesser/src/api/pokeApi.js
@@ -1,7 +1,23 @@
 export async function getPokemonByName(name) {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
-    if (!res.ok) {
+     if (!res.ok) {
         throw new Error(`Could not fetch pokemon: ${name}`);
+    }
+    return res.json();
+}
+
+export async function getPokemonByType(type) {
+    const res = await fetch(`https://pokeapi.co/api/v2/type/${type.toLowerCase()}`);
+    if (!res.ok) {
+        throw new Error(`Could not fetch pokemon: ${type}`);
+    }
+    return res.json();
+}
+
+export async function getPokemonByAmmount(number) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${number}}`);
+    if (!res.ok) {
+        throw new Error(`Could not fetch pokemon by ammount`);
     }
     return res.json();
 }
